803e3646f5a661fbf9ec4e8e5ad6bb0c8c0fcdcd
Luis Felipe Cano A <lfcano93@gmail.com>
2024-01-18 19:34:06 -0500

    [ui] -> AddedUIAndXmlForMoviesAndMoviesDetail also AddConfigurationBasicForGradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.example.imissher"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.imissher"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -35,18 +35,42 @@ android {
         sourceCompatibility = org.gradle.api.JavaVersion.VERSION_17
         targetCompatibility = org.gradle.api.JavaVersion.VERSION_17
     }
+
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //hilts
     implementation("com.google.dagger:hilt-android:2.44")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
+
+    //retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    //okhttp client
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
+    //okhttp interceptor
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    //gson
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    //Image loading
+    implementation("io.coil-kt:coil:2.5.0")
 }
 
 // Allow references to generated code
